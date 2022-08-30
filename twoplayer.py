a5c3c8553c6ee82d2afff1c75c3283341d9cffe8
shua <86475694+shuu-wasseo@users.noreply.github.com>
2022-08-30 12:20:46 +0800

    Update and rename functions.py to twoplayer.py

--- twoplayer.py
+++ twoplayer.py
@@ -1,17 +1,3 @@
-# prints a tic-tac-toe board, board takes a string argument filled with characters "o", "x" and " "
-def tictacboard(oxb):
-  board = f"""   1     2     3
-      |     |     
-a  {oxb[0]}  |  {oxb[1]}  |  {oxb[2]}  
- _____|_____|_____
-      |     |     
-b  {oxb[3]}  |  {oxb[4]}  |  {oxb[5]}  
- _____|_____|_____
-      |     |     
-c  {oxb[6]}  |  {oxb[7]}  |  {oxb[8]}  
-      |     |     """
-  return board
-
 # 2-player mode
 def twoplayer():
   win = False # status of whether game is over or not
